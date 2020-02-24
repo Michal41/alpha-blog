6a4346e251a7b2a540ab3d42933beb1bf6548be0
Fineax <michal.kanarek@gmail.com>
2020-02-24 20:27:43 +0100

    Add flash and error messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,13 +8,22 @@ class ArticlesController < ApplicationController
 		#render plain: params[:article]
 		@article = Article.new(article_params)
 		@article.save
-		
+		if @article.save
+			flash[:notice]='Article was succesly created'
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@article = Article.find(params[:id])
 	end
 
 	private
 		def article_params
 			params.require(:article).permit(:title, :description)
-			
+
 		end
 
 end
