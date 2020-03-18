19370c7a4e82948f2847e9f8034e6c90e7ddd800
Fineax <michal.kanarek@gmail.com>
2020-03-18 19:45:04 +0100

    Add categories model and test

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ ruby '2.5.1'
 gem 'rails', '~> 5.2.0'
 # Use sqlite3 as the database for Active Record
 
+gem 'rails-controller-testing'
+
 gem 'bcrypt', '~> 3.1.7'
 
 gem 'will_paginate','3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   post "users", to: 'users#create'
   resources :users, except: [:new]
   resources :articles
-
+  resources :categories, except: [:destroy]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
 end
